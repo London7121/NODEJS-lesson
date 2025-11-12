7a4fbc2f305e9d886bbbef410c0ce59a30839bec
Kamol Juraev <kamoljuraev7721@gmail.com>
2025-11-12 18:49:22 +0500

    HTML connect

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const http = require('http');
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
-
-    // req -> serverga so'rov
-    // res -> server javobi
-
-
-    console.log(req.url)
-    // res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('<h1>Hello, World!</h1>');
-    res.end();
-
+    if (req.method === "GET") {
+        res.writeHead(200, "Successfully", { 'content-type': 'text/html' })
+        if (req.url === "/") {
+            fs.readFile(path.join(__dirname, "templates", "index.html"), 'utf-8', (err, content) => {
+                if (err) throw err
+                res.end(content)
+            })
+        } else if (req.url === "/about") {
+            fs.readFile(path.join(__dirname, "templates", "about.html"), 'utf-8', (err, content) => {
+                if (err) throw err
+                res.end(content)
+            })
+        } else if (req.url === "/contact") {
+            fs.readFile(path.join(__dirname, "templates", "contact.html"), 'utf-8', (err, content) => {
+                if (err) throw err
+                res.end(content)
+            })
+        }
+    } else if (req.method === "POST") {
+        const body = []
+        res.writeHead(200, "Successfully",
+            {
+                'content-type': 'text/html; charset=uts-8'
+            }
+        )
+        req.on("data", data => body.push(Buffer.from(data).toString()))
+        req.on("end", () => {
+            const message = body.toString().split('=')[1]
+            res.end(`Email succssfully ${message}`)
+        })
+    }
 });
 
 server.listen(3000, () => {
